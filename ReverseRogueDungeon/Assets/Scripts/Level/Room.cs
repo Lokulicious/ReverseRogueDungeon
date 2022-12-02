4e2aa5c5924c80271f2e897e4aacae5ad408d23f
Nick Bohnsack <nick.bohnsack@gmail.com>
2022-12-02 23:25:12 +0100

    Created Player HealthController

---
+++ ReverseRogueDungeon/Assets/Scripts/Level/Room.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(fileName = "Room", menuName = "Level/Room", order = 1)]
+public class Room : ScriptableObject
+{
+    public Scene room;
+
+}
---
+++ ReverseRogueDungeon/Assets/Scripts/Player/Controllers/HealthController.cs
@@ -0,0 +1,67 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    [Title("Settings")]
+    [SerializeField] int maxHealth;
+    private int health;
+    [SerializeField][ColorPalette] Color damageColor;
+    [SerializeField] private float damageAnimationTime;
+    [SerializeField][ColorPalette] Color healColor;
+    [SerializeField] private float healAnimationTime;
+
+
+    [Title("References")]
+    [SerializeField] GameObject HealthBar;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ResetHealth();
+    }
+
+
+    public void Damage(int amount)
+    {
+        //deduct damage from health
+        health -= amount;
+        ColorCharacter(damageAnimationTime, damageColor);
+    }
+
+    public void Heal(int amount)
+    {
+        //add heal amount to health
+        health += amount;
+        ColorCharacter(healAnimationTime, healColor);
+    }
+
+    public void ResetHealth()
+    {
+        health = maxHealth;
+    }
+
+    public bool IsAlive()
+    {
+        //checks if health is above 0 and returns weather it is true
+        if (health > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    IEnumerator ColorCharacter(float colorTime, Color color)
+    {
+        spriteRenderer.color = color;
+        yield return new WaitForSeconds(colorTime);
+        spriteRenderer.color = Color.white;
+    }
+}
